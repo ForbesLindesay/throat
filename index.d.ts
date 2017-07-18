04a6a74cece8c775e4aa0a696089460254e1c87a
Blake Embrey <hello@blakeembrey.com>
2017-07-17 21:05:48 -0400

    Add `promise` use overloads

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,9 @@
-declare function throat<TResult, TFn extends (...args: Array<any>) => Promise<TResult>>(size: number, fn: TFn): TFn;
-declare function throat<TResult, TFn extends (...args: Array<any>) => Promise<TResult>>(fn: TFn, size: number): TFn;
-declare function throat(size: number): <TResult>(fn: () => Promise<TResult>) => Promise<TResult>;
+declare const throat: Throttle & { (p: typeof Promise): Throttle };
+
+interface Throttle {
+    <TResult, TFn extends (...args: Array<any>) => Promise<TResult>>(size: number, fn: TFn): TFn;
+    <TResult, TFn extends (...args: Array<any>) => Promise<TResult>>(fn: TFn, size: number): TFn;
+    (size: number): <TResult>(fn: () => Promise<TResult>) => Promise<TResult>;
+}
 
 export = throat;
