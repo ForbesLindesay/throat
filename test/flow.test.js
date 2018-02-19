e578ce6cf265f3ad04eb031e8bbe1aad8374e778
Forbes Lindesay <forbes@lindesay.co.uk>
2018-02-19 10:44:07 +0000

    Remove absolute file paths from flow snapshot

--- test/flow.test.js
+++ test/flow.test.js
@@ -3,5 +3,12 @@ const spawnSync = require('child_process').spawnSync;
 test('flow', () => {
   const result = spawnSync('npm', ['run', 'flow']);
   expect(result.status).not.toBe(0);
-  expect(result.stdout.toString('utf8').split('\n').filter(l => !/^\>/.test(l)).join('\n')).toMatchSnapshot();
+  expect(
+    result.stdout
+      .toString('utf8')
+      .replace(/\/[a-z0-9_\/\\\.]+\/([a-z]+\.js)/g, '<absolute-dir>$1')
+      .split('\n')
+      .filter(l => !/^\>/.test(l))
+      .join('\n')
+  ).toMatchSnapshot();
 });
