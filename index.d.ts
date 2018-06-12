1142bb1365dc65f90e9c025281a4e77c92894dcf
Michael Stillwell <mjs@beebo.org>
2018-06-12 13:01:00 +0100

    Add comment explaining how to use the typings
    Various other common patterns either produce syntax errors or fail to work as expected, such as:
    
    * `const throat = require('throat');`
    * `import * as throat from 'throat';`
    * `import {throat} from 'throat';`

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,4 @@ declare function throat<TResult, TFn extends (...args: Array<any>) => Promise<TR
 declare function throat<TResult, TFn extends (...args: Array<any>) => Promise<TResult>>(fn: TFn, size: number): TFn;
 declare function throat(size: number): <TResult>(fn: () => Promise<TResult>) => Promise<TResult>;
 
-export = throat;
+export = throat; // get typings via: import throat = require("throat")
