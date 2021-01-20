ff63aec58793d8954e86c84722443230470b34fe
Forbes Lindesay <forbes@lindesay.co.uk>
2021-01-20 19:57:00 +0000

    perf: do not put closures on the queue
    This makes the benchmarks approximately match version 5.0.0

--- perf.js
+++ perf.js
@@ -1,15 +1,29 @@
 const throat = require('./index');
 
+const MAX_COUNT = 1000000;
+const ITERATIONS = 10;
 const promises = [];
-for (let i = 0; i < 1000000; i++) {
-  promises.push(() => new Promise(resolve => process.nextTick(resolve)));
+for (let i = 0; i < MAX_COUNT; i++) {
+  promises.push(() => new Promise((resolve) => process.nextTick(resolve)));
 }
 
 Promise.resolve().then(async () => {
-  for (let amount = 10; amount <= 1000000; amount = amount * 10) {
+  console.log('limit=10');
+  for (let amount = 10; amount <= MAX_COUNT; amount = amount * 10) {
     const list = promises.slice(0, amount);
     console.time(amount + ' promises');
-    await Promise.all(list.map(throat(10, fn => fn())));
+    for (let i = 0; i < ITERATIONS; i++) {
+      await Promise.all(list.map(throat(10, (fn) => fn())));
+    }
+    console.timeEnd(amount + ' promises');
+  }
+  console.log('limit=1000000');
+  for (let amount = 10; amount <= MAX_COUNT; amount = amount * 10) {
+    const list = promises.slice(0, amount);
+    console.time(amount + ' promises');
+    for (let i = 0; i < ITERATIONS; i++) {
+      await Promise.all(list.map(throat(1000000, (fn) => fn())));
+    }
     console.timeEnd(amount + ' promises');
   }
 });
